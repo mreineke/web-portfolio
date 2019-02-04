0b6c388538af2ff021b4160b840f4d0f774697c0
mreineke <mreineke20@pascack.org>
2019-02-04 10:48:54 -0500

    Multiple Pages added

--- deploy.py
+++ deploy.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
    return render_template("index.html")
+@app.route("/java")
+def java():
+    return render_template("java.html")
+@app.route("/python")
+def python():
+    return render_template("python.html")
+@app.route("/swift")
+def swift():
+    return render_template("swift.html")
+@app.route("/webdev")
+def webdev():
+    return render_template("webdev.html")
 if __name__ == "__main__":
    app.run(debug=True)
